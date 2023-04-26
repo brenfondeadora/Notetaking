36aded62c0765bd9398cec31d79c099ffcbd5de9
Brenda SC <brenda.saavedra@fondeadora.com>
2023-04-26 14:26:36 -0600

    refactor: remove unused action deleteItems

--- Notetaking/ContentView.swift
+++ Notetaking/ContentView.swift
@@ -31,7 +31,6 @@ struct ContentView: View {
                         }
                     }
                 }
-                .onDelete(perform: deleteItems)
             }
             .toolbar {
                 ToolbarItem {
@@ -41,23 +40,6 @@ struct ContentView: View {
                 }
             }
             Text("Select a note")
-        }
-    }
-
-    
-
-    private func deleteItems(offsets: IndexSet) {
-        withAnimation {
-            offsets.map { items[$0] }.forEach(viewContext.delete)
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
         }
     }
 }
